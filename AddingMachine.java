c9efc9aae047ec7792574fbe88e8b1a44237beae
JohnMarkley <johnwilliammarkley@gmail.com>
2019-10-03 22:43:17 -0700

    Add files via upload

---
+++ AddingMachine.java
@@ -0,0 +1,58 @@
+/**
+ * Name: John William Markley
+ * Class ID: 70641 | CSE 360 Monday 9:40
+ * Description: This program declares a Palindrome object that will construct with a String,
+ * 			and a method that will go through the string to determine if it is a Palindrome and return
+ * 			a boolean accordingly. 
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	/**
+	 * Constructor for Adding Machine
+	 */
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/**
+	 * getTotal
+	 * @return
+	 */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * Blank add function
+	 * @param value
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * Blank subtract function
+	 * @param value
+	 */
+	public void subtract (int value) {
+		
+	}
+	/**
+	 * 	toString method
+	 * @return "" 
+	 * */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * clear method
+	 */
+	public void clear() {
+	
+	}
+}
