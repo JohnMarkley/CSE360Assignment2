c05657181c486a0dfde06b4b527913cea8ed7ae7
JohnMarkley <johnwilliammarkley@gmail.com>
2019-10-07 17:03:19 -0700

    Add files via upload

--- AddingMachine.java
+++ AddingMachine.java
@@ -1,58 +1,70 @@
 /**
  * Name: John William Markley
  * Class ID: 70641 | CSE 360 Monday 9:40
- * Description: This program declares a Palindrome object that will construct with a String,
- * 			and a method that will go through the string to determine if it is a Palindrome and return
- * 			a boolean accordingly. 
+ * Description: This program is a calculator that tracks all operations in a StringBuilder while containing the 
+ * total result in an integer
+ * Github Repo: https://github.com/JohnMarkley/CSE360Assignment2
  */
 package cse360assign2;
 
 public class AddingMachine {
 
 	private int total;
+	private StringBuilder history;
+	
 	/**
-	 * Constructor for Adding Machine
+	 * Constructor for Adding Machine that creates a stringbuilder object 
+	 * & initializes total to 0
 	 */
+	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = new StringBuilder();
 	}
 	
 	/**
-	 * getTotal
-	 * @return
+	 * Return total 
+	 * @return total 
 	 */
 	
 	public int getTotal () {
-		return 0;
+		return this.total;
 	}
 	
 	/**
-	 * Blank add function
+	 * Add parameter value to total 
 	 * @param value
 	 */
+	
 	public void add (int value) {
-		
+		this.total += value;
+		this.history.append(" + " + value);
 	}
 	
 	/**
-	 * Blank subtract function
+	 * Subtract parameter value to total
 	 * @param value
 	 */
+	
 	public void subtract (int value) {
-		
+		this.total -= value;
+		this.history.append(" - " + value);
 	}
+	
 	/**
-	 * 	toString method
-	 * @return "" 
+	 * 	toString method that shows history of calculations
+	 * @return "0" + StringBuilder History
 	 * */
+	
 	public String toString () {
-		return "";
+		return "0" + history.toString();
 	}
 
 	/**
-	 * clear method
+	 * clear method that erases all history
 	 */
-	public void clear() {
 	
+	public void clear() {
+		this.history.setLength(0);
 	}
 }
